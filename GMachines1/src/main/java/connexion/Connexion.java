b71ffa506ac0f64f5b13a456ab1388dc57340966
LENOVO <LENOVO@DESKTOP-MMJAVPR>
2022-01-22 00:45:44 +0100

    Commit

--- GMachines1/src/main/java/connexion/Connexion.java
+++ GMachines1/src/main/java/connexion/Connexion.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 
 public class Connexion {
 
-    private static String login = "root";
+
+    /*private static String login = "root";
     private static String password = "";
     private static String url = "jdbc:mysql://localhost/jdbc?serverTimezone=UTC";
     private Connection connection = null;
@@ -15,6 +16,24 @@ public class Connexion {
     private Connexion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, login, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver introvable");
+        } catch (SQLException e) {
+            System.out.println("Connexion errror");
+            System.out.println(e.getMessage());
+        }
+    }*/
+	
+    private static String login = "gjpdiellrhotuu";
+    private static String password = "eda00f47f93dc0f1a5d656c52d22c360449cd5cddfd22925df27e74475f47dcd";
+    private static String url = "jdbc:postgresql://ec2-3-222-49-168.compute-1.amazonaws.com/d3folj5e36r3p5?serverTimezone=UTC";
+    private Connection connection = null;
+    private static Connexion instane;
+
+    private Connexion() {
+        try {
+            Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(url, login, password);
         } catch (ClassNotFoundException e) {
             System.out.println("Driver introvable");
